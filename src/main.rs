dcce66f78aba5730edb97e79bff4deabcacbc654
Samir Mammadov <samir@droplit.io>
2018-08-14 17:56:10 -0400

    init

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+
+
+use std::collections::HashMap;
+
+let mut contacts = HashMap::new();
+
+contacts.insert(String::from("Farid M"), 10);
+contacts.insert(String::from("Richard L"), 50);
+
+fn main() {
+    println!("Hello, world!");
+}
