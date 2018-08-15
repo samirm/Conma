7cd057e4fa70c52a36629de987bd56f8eee1487a
Samir Mammadov <samir@droplit.io>
2018-08-15 18:03:47 -0400

    updated data model, expanding skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,44 @@
+extern crate chrono;
 
-
+use chrono::prelude::*;
 use std::collections::HashMap;
 
-let mut contacts = HashMap::new();
+enum Tier {
+	one,
+	two,
+	three,
+}
 
-contacts.insert(String::from("Farid M"), 10);
-contacts.insert(String::from("Richard L"), 50);
+struct Person {
+	name: String,
+	tier: Tier,
+}
+
+static contacts:HashMap<Person, DateTime<Local>> = HashMap::new();
 
 fn main() {
     println!("Hello, world!");
+
+	contacts.insert(Person { name: "Farid M", tier: Tier::one }, Local::now());
+	contacts.insert(Person { name: "Richard L", tier: Tier::one }, Local::now());
+	
+	listContacts();
+}
+
+fn listContacts() {
+	for (person, date) in &contacts {
+		println!("{}: \"{}\"", person, date);
+	}
+}
+
+fn updateContact(person: Person) {
+	if contacts.insert(person, Local::now()) != None {
+		println!("Contact updated successfully.");
+	} else {
+		println!("Contact does not exist.")
+	}
+}
+
+fn updateContactTier(name: String, tier: String) {
+	
 }
