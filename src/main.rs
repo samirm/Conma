617f97132f91163b17e1ebae68b9a8232fc77cae
Samir Mammadov <samir@droplit.io>
2018-08-16 17:47:02 -0400

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,52 @@
 extern crate chrono;
+extern crate termion;
 
 use chrono::prelude::*;
 use std::collections::HashMap;
+use std::fs::File;
+use std::path::Path;
 
 enum Tier {
-	one,
-	two,
-	three,
+    One,
+    Two,
+	Three,
 }
 
 struct Person {
 	name: String,
 	tier: Tier,
+    pmoc: [String],
 }
 
-static contacts: HashMap<Person, DateTime<Local>> = HashMap::new();
+static CONTACTS: HashMap<Person, DateTime<Local>> = HashMap::new();
 
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to Conma, what would you like to do?");
 
-	contacts.insert(Person { name: "Farid M".to_string(), tier: Tier::one }, Local::now());
-	contacts.insert(Person { name: "Richard L".to_string(), tier: Tier::one }, Local::now());
+	CONTACTS.insert(Person { name: "Farid M".to_string(), tier: Tier::One, pmoc: *["Whatsapp".to_string()] }, Local::now());
+	CONTACTS.insert(Person { name: "Richard L".to_string(), tier: Tier::One, pmoc: *["Telegram".to_string()] }, Local::now());
 	
-	listContacts();
+	list_contacts();
 }
 
-fn listContacts() {
-	for (person, date) in &contacts {
+fn list_contacts() {
+	for (person, date) in &CONTACTS {
 		println!("{}: \"{}\"", person, date);
 	}
 }
 
-fn updateContact(person: Person) {
-	if contacts.insert(person, Local::now()) != None {
+fn update_contact((name, tier, pmoc): (String, String, String)) {
+	if CONTACTS.insert(person, Local::now()) != None {
 		println!("Contact updated successfully.");
 	} else {
 		println!("Contact does not exist.")
 	}
 }
 
-fn updateContactTier(name: String, tier: String) {
+fn update_contact_tier(name: String, tier: String) {
 	
+}
+
+fn import_contacts(file: String) {
+
 }
