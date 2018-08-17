7081e16ad92bf2456f92b9439ae506dd2046734b
Samir Mammadov <samir@droplit.io>
2018-08-17 16:59:31 -0400

    adjusting data model, implementing check_timezone and main menu

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use chrono::prelude::*;
 use std::collections::HashMap;
 use std::fs::File;
 use std::path::Path;
+use std::io;
 
 enum Tier {
     One,
@@ -15,17 +16,41 @@ enum Tier {
 struct Person {
 	name: String,
 	tier: Tier,
-    pmoc: [String],
+    pmoc: Vec<String>,
+    timeZone: i8, //UTC
 }
 
-static CONTACTS: HashMap<Person, DateTime<Local>> = HashMap::new();
+static CONTACTS: HashMap<Person, Date<Local>> = HashMap::new();
+static CURRENT_TIME_ZONE: i8 = undefined;
+
+fn check_timezone() {
+    let acceptableRange = -12..12;
+    if CURRENT_TIME_ZONE == undefined {
+        println!("What UTC offset are you in?");
+
+        loop {
+            io::stdin().read_line(&mut input).expect("Failed to read line.");
+
+            CURRENT_TIME_ZONE = match input.trim().parse() {
+                Ok(num) => num,
+                Err(..) => println!("Please enter a negative or positive integer (e.g. -12 .. 12)"),
+            };
+        }
+    }
+}
 
 fn main() {
     println!("Welcome to Conma, what would you like to do?");
+    println!("\n 1. Update contact(s).");
+    println!("\n 2. Import contact(s).");
+    println!("\n 3. View calendar.");
+    println!("\n 0. Quit.");
 
-	CONTACTS.insert(Person { name: "Farid M".to_string(), tier: Tier::One, pmoc: *["Whatsapp".to_string()] }, Local::now());
-	CONTACTS.insert(Person { name: "Richard L".to_string(), tier: Tier::One, pmoc: *["Telegram".to_string()] }, Local::now());
-	
+	CONTACTS.insert(Person { name: "Farid M".to_string(), tier: Tier::One,
+        pmoc: *["WA".to_string()], timeZone: -5 }, Local::now().date());
+	CONTACTS.insert(Person { name: "Richard L".to_string(), tier: Tier::One,
+        pmoc: *["TG".to_string()], timeZone: -5  }, Local::now().date());
+
 	list_contacts();
 }
 
@@ -36,7 +61,7 @@ fn list_contacts() {
 }
 
 fn update_contact((name, tier, pmoc): (String, String, String)) {
-	if CONTACTS.insert(person, Local::now()) != None {
+	if CONTACTS.insert(person, Local::now().date()) != None {
 		println!("Contact updated successfully.");
 	} else {
 		println!("Contact does not exist.")
